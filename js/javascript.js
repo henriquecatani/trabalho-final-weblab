ee54e198c6931f5c734d68f20084c0a6754df140
henriquecatani <catanihenrique22@gmail.com>
2025-06-26 11:41:50 -0300

    Adicionado botao acessibilidade nas outras páginas, adicionados comentários no código, adicionado página em construção e temas nos ícones.

--- js/javascript.js
+++ js/javascript.js
@@ -2,17 +2,28 @@
 let trilho = document.getElementById("trilho")
 let escuro = document.querySelector('body')
 let indicador = document.getElementById("themeToggleImg")
+let btnAcessibilidade = document.getElementById("botaoAcessibilidade")
+let btnSubmit = document.getElementById("enviar")
+let btnVoltar = document.getElementById("botaoVoltar")
 trilho.addEventListener('click', ()=>{
         trilho.classList.toggle('light') // muda o tema do toggle
         escuro.classList.toggle('light') // muda o tema geral
-        if (indicador.src.endsWith("/img/light_mode.png")) { // muda o tema do icone no toggle, alterando a fonte da imagem de light pra dark e vice versa
+        if (indicador.src.endsWith("/img/light_mode.png")) { // muda o tema dos icones, alterando a fonte da imagem de light pra dark e vice versa
                 indicador.src='/img/dark_mode.png'
+                btnAcessibilidade.src='/img/acessibilidadeW.png'
+                btnVoltar.src='/img/backButtonW.png'
         }
-        else{ // quando tiver icone dark
+        else{ // quando estiver dark
                 indicador.src='/img/light_mode.png'
+                btnAcessibilidade.src='/img/acessibilidadeB.png'
+                btnVoltar.src='/img/backButtonB.png'
         }
 })
 
 function ativaAcessibilidade() {
         document.body.classList.toggle('acessibilidade')
 }
+
+btnSubmit.addEventListener('click', ()=>{
+        window.location.href = "/html/construcao.html";
+})
