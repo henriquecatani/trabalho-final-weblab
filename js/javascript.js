e24c539fee5eb0ba9566a32a89560f3c2fee7bfc
henriquecatani <catanihenrique22@gmail.com>
2025-06-26 20:12:23 -0300

    atualização final

--- js/javascript.js
+++ js/javascript.js
@@ -8,14 +8,14 @@ trilho.addEventListener('click', ()=>{
         trilho.classList.toggle('light') // muda o tema do toggle
         escuro.classList.toggle('light') // muda o tema geral
         if (indicador.src.endsWith("/img/light_mode.png")) { // muda o tema dos icones, alterando a fonte da imagem de light pra dark e vice versa
-                indicador.src='/img/dark_mode.png'
-                btnAcessibilidade.src='/img/acessibilidadeW.png'
-                btnVoltar.src='/img/backButtonW.png'
+                indicador.src='./img/dark_mode.png'
+                btnAcessibilidade.src='./img/acessibilidadeW.png'
+                btnVoltar.src='./img/backButtonW.png'
         }
         else{ // quando estiver dark
-                indicador.src='/img/light_mode.png'
-                btnAcessibilidade.src='/img/acessibilidadeB.png'
-                btnVoltar.src='/img/backButtonB.png'
+                indicador.src='./img/light_mode.png'
+                btnAcessibilidade.src='./img/acessibilidadeB.png'
+                btnVoltar.src='./img/backButtonB.png'
         }
 })
 
